c8e5624a34efee1e13490707c8429e54cfb84593
Joon <77248354+joonsauce@users.noreply.github.com>
2023-10-25 09:39:13 -0400

    update linalg lib

--- linalg.hpp
+++ linalg.hpp
@@ -1,3 +1,23 @@
 #include <vector>
 
+#include "imaginary.hpp"
+
 using namespace std;
+
+typedef vector<Imaginary> Row;
+
+vector<Row> empty = {};
+
+class Matrix {
+private:
+    vector<Row> rows;
+    void swapRows(Row * row1, Row* row2);
+    bool verifyMatrix();
+    bool isDiagonal();
+    bool isRREF();
+    // void sortMatrix(int firstIndex);
+public:
+    vector<Row> getMatrix();
+    void addRowToMatrix(Row row);
+    void doRREF();
+};
